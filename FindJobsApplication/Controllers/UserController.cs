e949d7ff4062cd4ee8dfb0ae3aad20357c34e14b
Raikungfu <raikungfu@gmail.com>
2024-11-07 08:06:17 +0700

    profile

--- FindJobsApplication/Controllers/UserController.cs
+++ FindJobsApplication/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 using Microsoft.IdentityModel.Tokens;
+using System.Data;
 using System.Security.Claims;
 
 namespace FindJobsApplication.Controllers
@@ -27,41 +28,79 @@ namespace FindJobsApplication.Controllers
         }
 
         [HttpGet("profile")]
-        public IActionResult GetProfile()
+        public IActionResult GetProfile(int? userId)
         {
-            var claimRole = User.FindFirst(ClaimTypes.Role)?.Value;
-            var claimValue = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-            if (claimRole.IsNullOrEmpty() || string.IsNullOrEmpty(claimValue) || !Enum.TryParse(claimRole, out UserType role) || !int.TryParse(claimValue, out int userId))
+            if (userId == null)
             {
-                return Unauthorized("User not logged in. Please log in to continue.");
-            }
+                var claimRole = User.FindFirst(ClaimTypes.Role)?.Value;
+                var claimValue = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            switch (role)
-            {
-                case UserType.Employee:
-                    var employee = _unitOfWork.Employee.GetFirstOrDefault(x => x.UserId == userId);
-                    if (employee == null)
-                    {
-                        return NotFound();
-                    }
-                    return Ok(employee);
-                case UserType.Employer:
-                    var employer = _unitOfWork.Employer.GetFirstOrDefault(x => x.UserId == userId);
-                    if (employer == null)
-                    {
-                        return NotFound();
-                    }
-                    return Ok(employer);
-                case UserType.Admin:
-                    var admin = _unitOfWork.Admin.GetFirstOrDefault(x => x.UserId == userId);
-                    if (admin == null)
-                    {
-                        return NotFound();
-                    }
-                    return Ok(admin);
-                default:
+                if (claimRole.IsNullOrEmpty() || string.IsNullOrEmpty(claimValue) || !Enum.TryParse(claimRole, out UserType role) || !int.TryParse(claimValue, out int userId))
+                {
                     return Unauthorized("User not logged in. Please log in to continue.");
+                }
+
+                switch (role)
+                {
+                    case UserType.Employee:
+                        var employee = _unitOfWork.Employee.GetFirstOrDefault(x => x.UserId == userId);
+                        if (employee == null)
+                        {
+                            return NotFound();
+                        }
+                        return Ok(employee);
+                    case UserType.Employer:
+                        var employer = _unitOfWork.Employer.GetFirstOrDefault(x => x.UserId == userId);
+                        if (employer == null)
+                        {
+                            return NotFound();
+                        }
+                        return Ok(employer);
+                    case UserType.Admin:
+                        var admin = _unitOfWork.Admin.GetFirstOrDefault(x => x.UserId == userId);
+                        if (admin == null)
+                        {
+                            return NotFound();
+                        }
+                        return Ok(admin);
+                    default:
+                        return Unauthorized("User not logged in. Please log in to continue.");
+                }
+            }
+            else
+            {
+                var user = _unitOfWork.User.GetFirstOrDefault(x => x.UserId == userId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                switch (user.UserType)
+                {
+                    case UserType.Employee:
+                        var employee = _unitOfWork.Employee.GetFirstOrDefault(x => x.UserId == userId);
+                        if (employee == null)
+                        {
+                            return NotFound();
+                        }
+                        return Ok(employee);
+                    case UserType.Employer:
+                        var employer = _unitOfWork.Employer.GetFirstOrDefault(x => x.UserId == userId);
+                        if (employer == null)
+                        {
+                            return NotFound();
+                        }
+                        return Ok(employer);
+                    case UserType.Admin:
+                        var admin = _unitOfWork.Admin.GetFirstOrDefault(x => x.UserId == userId);
+                        if (admin == null)
+                        {
+                            return NotFound();
+                        }
+                        return Ok(admin);
+                    default:
+                        return Unauthorized("User not logged in. Please log in to continue.");
+                }
             }
         }
     }
